59b2040b3d6af08d92ae8a4e74a116677de2d19c
Bryce Vandegrift <bryce@brycevandegrift.xyz>
2024-03-19 11:36:22 -0400

    Update dwm to version 6.5
    Manually added updated code to dwm and bumped
    the version to 6.5

--- config.h
+++ config.h
@@ -90,7 +90,8 @@ static const Layout layouts[] = {
 #define STATUSBAR "dwmblocks"
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_tan, "-sf", col_gray1, "-c", "-l", "30", NULL };
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_tan, "-sf", col_gray1, "-c", "-l", "30", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[] = { "firefox-esr", NULL };
 static const char *emailcmd[] = { "st", "-e", "aerc", NULL };
